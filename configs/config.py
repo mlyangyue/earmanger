c628610798b9dbcf6a3b936aafe4196c8d0e6fa6
wangranming <wangranming1989@gmail.com>
2018-06-26 14:51:21 +0800

    修改七牛配置，h5域名

--- configs/config.py
+++ configs/config.py
@@ -20,11 +20,18 @@ class Config():
 
 	}
 	#七牛配置
+	# QINIU = {
+	# 	"access_key":"B_g75tCn6AMDtd8FLkQNbCftre4MOwAMSeYC46za",
+	# 	"secret_key":"RMOpLjlN1aLWjq2ZUSeNQNSYUTp07umQBU2HQKqz",
+	# 	"bucket_name":"if-will",
+	# 	"qn_domain_name":"http://7u2mbm.com1.z0.glb.clouddn.com/"
+	# }
+
 	QINIU = {
-		"access_key":"B_g75tCn6AMDtd8FLkQNbCftre4MOwAMSeYC46za",
-		"secret_key":"RMOpLjlN1aLWjq2ZUSeNQNSYUTp07umQBU2HQKqz",
-		"bucket_name":"if-will",
-		"qn_domain_name":"http://7u2mbm.com1.z0.glb.clouddn.com/"
+		"access_key":"HZLRx-_I8byU84yRe5HYNjjAdWSfsnFS_jdLE1An",
+		"secret_key":"WWW3riM45S-iXSpvM9maAE0wipnK56Mvr9r2-n9K",
+		"bucket_name":"iterduoapp",
+		"qn_domain_name":"http://pawtn3z0h.bkt.clouddn.com/"
 	}
 	template_folder = 'templates'
 	basedir = os.path.abspath(os.path.dirname(__file__))
@@ -67,11 +74,11 @@ class ProductConfig(Config):
 	# PYTHON reids
 	PYTHON_REDIS_URL = "redis://127.0.0.1:6379/0"
 	# 后台数据库地址
-	SQLALCHEMY_DATABASE_URI = "mysql://iterduo:iterduo@114.24.72.141:3306/earfin"
+	SQLALCHEMY_DATABASE_URI = "mysql://iterduo:iterduo@60.205.227.109:3306/earfin"
 	# 图片服务器
 	IMAGE_DIR = "/data/share/images/"
 	SQLALCHEMY_BINDS = {
-		'backendrdb':"mysql://iterduo:iterduo@114.24.72.141:3306/earfin"
+		'backendrdb':"mysql://iterduo:iterduo@60.205.227.109:3306/earfin"
 	}
 	# SQLALCHEMY_ECHO = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
--- configs/constants.py
+++ configs/constants.py
@@ -6,4 +6,4 @@ __author__ = 'Andy'
 CRYPTE  = "jiami_rand123"
 
 """新闻url"""
-NEWSURL = "http://118.24.72.141/earfinance/earFinance.html?newsId="
+NEWSURL = "http://www.iterduo.cn:5001/earfinance/earFinance.html?newsId="
